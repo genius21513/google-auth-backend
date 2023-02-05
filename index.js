489595f39edcf4a2ec34dc19008b94400c33b233
Genius <genius21513@gmail.com>
2023-02-05 23:37:50 +0900

    Modified

--- index.js
+++ index.js
@@ -42,6 +42,6 @@ app.get('/auth/callback/failure', (req, res) => {
     res.send("Error");
 })
 
-app.listen(80, () => {
+app.listen(4000, () => {
     console.log("Server Running on port 4000");
 });
